ca8be15e4ea2163256f021de260521b4960e1892
Felipe Cavani <fcavani@gmail.com>
2015-01-09 08:54:59 -0200

    Changed the copyright in the file header.

--- math/round/round_test.go
+++ math/round/round_test.go
@@ -1,4 +1,7 @@
-// Copyright 2009 Felipe Alves Cavani. All rights reserved.
+// Copyright 2015 Felipe A. Cavani. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Start date:        2014-04-09
 // Last modification: 2014-
 
--- math/round/rounddec.go
+++ math/round/rounddec.go
@@ -1,4 +1,7 @@
-// Copyright 2009 Felipe Alves Cavani. All rights reserved.
+// Copyright 2015 Felipe A. Cavani. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Start date:        2014-04-01
 // Last modification: 2014-
 
@@ -21,7 +24,7 @@ func RoundDec32(x float32, prec int) float32 {
 	}
 	dec1 := float32(math.Pow10(prec))
 	dec2 := float32(math.Pow10(prec + 1))
-	tmp := int(x * dec1) //3141
+	tmp := int(x * dec1)
 	last := int(x*dec2) - tmp*10
 	if norm(last) >= 5 && last >= 0 {
 		tmp += 1
@@ -37,7 +40,7 @@ func RoundDec64(x float64, prec int) float64 {
 	}
 	dec1 := math.Pow10(prec)
 	dec2 := math.Pow10(prec + 1)
-	tmp := int(x * dec1) //3141
+	tmp := int(x * dec1)
 	last := int(x*dec2) - tmp*10
 	if norm(last) >= 5 && last >= 0 {
 		tmp += 1
