6eee9a8106d5f6e70fee526781572b39faa1efea
Felipe Cavani <fcavani@gmail.com>
2015-01-18 13:57:07 -0200

    More comments.

---
+++ util.go
@@ -0,0 +1,7 @@
+// Copyright 2013 Felipe Alves Cavani. All rights reserved.
+// Start date:		2014-08-08
+// Last modification:	2014-x
+
+// Util package have a collection of functions to help standard lib
+// do some trivial stuffs.
+package util
