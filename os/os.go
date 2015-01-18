e84ee975af5a55f1a1c95bb9c8ff53dfeb27416d
Felipe Cavani <fcavani@gmail.com>
2015-01-18 14:53:17 -0200

    more more more...

---
+++ os/os.go
@@ -0,0 +1,6 @@
+// Copyright 2015 Felipe A. Cavani. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Os package have some helpers os functions.
+package os
