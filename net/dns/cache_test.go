e3067991f9d2a919931780d2f741e216d94e2ced
Felipe Cavani <fcavani@gmail.com>
2015-01-20 09:38:31 -0200

    Simple lookup but if it fails try to retrieve the hosts from cache.

---
+++ net/dns/cache_test.go
@@ -0,0 +1,24 @@
+// Copyright 2015 Felipe A. Cavani. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package dns
+
+import (
+	"github.com/fcavani/e"
+	"testing"
+)
+
+func TestLocalHost(t *testing.T) {
+	addrs, err := LookupHostCache("localhost")
+	if err != nil {
+		t.Fatal(e.Trace(e.Forward(err)))
+	}
+	t.Log(addrs)
+	for _, addr := range addrs {
+		if addr != "::1" && addr != "127.0.0.1" {
+			t.Fatal("wrong address")
+		}
+	}
+	
+}
