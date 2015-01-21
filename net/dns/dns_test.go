ac0a9bdf39199a94609a035228fce9df358934a9
Felipe Cavani <fcavani@gmail.com>
2015-01-21 20:52:13 -0200

    Tests for the resolver...

---
+++ net/dns/dns_test.go
@@ -0,0 +1,26 @@
+// Copyright 2015 Felipe A. Cavani. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package dns
+
+import (
+	"github.com/fcavani/e"
+	"net/url"
+	"testing"
+)
+
+func TestResolveUrl(t *testing.T) {
+	url, err := url.Parse("http://localhost:8080/foo.html?q=search#fragment")
+	if err != nil {
+		t.Fatal("parse failed", err)
+	}
+	u, err := ResolveUrl(url)
+	if err != nil {
+		t.Fatal(e.Trace(e.Forward(err)))
+	}
+	if u.Host != "127.0.0.1:8080" && u.Host != "[::1]:8080" {
+		t.Fatal("can't resolve", u)
+	}
+	t.Log(u)
+}
