aeb2067d79e35e02b20fe833e71325a8dd2f34c7
Felipe Cavani <fcavani@gmail.com>
2015-01-21 21:12:39 -0200

    If only have the host part return it with a error.

--- net/net.go
+++ net/net.go
@@ -16,6 +16,24 @@ const ErrCantSplitHostPort = "can't split host port"
 const ErrCantFindHost = "can't find the host"
 const ErrCantFindPort = "can't find the port number"
 
+func IsValidIpv6(ip string) bool {
+	r, err := regexp.Compile(`\[([a-z0-9:]*)\]`)
+	if err != nil {
+		panic("can't compile ipv6 regexp")
+	}
+	x := r.FindAllStringSubmatch(ip, -1)
+	if len(x) == 0 {
+		return false
+	}
+	if len(x[0]) != 2 {
+		return false
+	}
+	if x[0][1] != strings.TrimSuffix(strings.TrimPrefix(ip, "["), "]") {
+		return false
+	} 
+	return true
+}
+
 // SplitHostPort splits a string with a ipv6, ipv4 or hostname with a port number.
 func SplitHostPort(hp string) (host, port string, err error) {
 	if len(hp) == 0 {
@@ -28,28 +46,42 @@ func SplitHostPort(hp string) (host, port string, err error) {
 			return "", "", e.Push(err, "can't compile ipv6 regexp")
 		}
 		x := r.FindAllStringSubmatch(hp, -1)
-		if len(x) <= 0 {
-			return "", "", e.New(ErrCantGetIp)
+		if len(x) == 0 {
+			if IsValidIpv6(hp) {
+				host = strings.TrimSuffix(strings.TrimPrefix(hp, "["), "]")
+				port = ""
+			} else {
+				return "", "", e.New(ErrCantGetIp)
+			}
+		} else {
+			if len(x[0]) == 2 {
+				host = x[0][1]
+				port = ""
+			} else if len(x[0]) == 3 {
+				host = x[0][1]
+				port = x[0][2]			
+			} else {
+				return "", "", e.New(ErrCantGetIp)
+			}
 		}
-		if len(x[0]) != 3 {
-			return "", "", e.New(ErrCantGetIp)
-		}
-		host = x[0][1]
-		port = x[0][2]
 	} else {
 		//ip4 and host name
 		ipport := strings.SplitN(hp, ":", 2)
-		if len(ipport) != 2 {
+		if len(ipport) == 1 {
+			host = ipport[0]
+			port = ""
+		} else if len(ipport) == 2 {
+			host = ipport[0]
+			port = ipport[1]
+		} else {
 			return "", "", e.New(ErrCantSplitHostPort)
 		}
-		host = ipport[0]
-		port = ipport[1]
 	}
 	if host == "" {
 		return "", "", e.New(ErrCantFindHost)
 	}
 	if port == "" {
-		return "", "", e.New(ErrCantFindPort)
+		return host, "", e.New(ErrCantFindPort)
 	}
 	return
 }
