810f1defd449640c8051e0b39e4fb998c7d7a747
Felipe Cavani <fcavani@gmail.com>
2015-01-26 13:13:51 -0200

    Add timeout to the send mail function.

--- net/smtp/smtp.go
+++ net/smtp/smtp.go
@@ -11,6 +11,7 @@ import (
 	"crypto/tls"
 	"fmt"
 	"github.com/fcavani/e"
+	"io"
 	"log"
 	"net"
 	"net/smtp"
@@ -18,70 +19,7 @@ import (
 	"time"
 )
 
-// Generate a comma separated list of e-mails from a array of e-mails
-func EmailsToString(mails []string) (s string) {
-	for i, mail := range mails {
-		if i > 0 {
-			s += ", "
-		}
-		s += mail
-	}
-	return
-}
-
-// SendMail send a message to specific destination (to) using smtp server in addrs
-// and a auth.
-func SendMail(addr string, a smtp.Auth, from string, to []string, hello string, msg []byte) error {
-	c, err := smtp.Dial(addr)
-	if err != nil {
-		return e.New(err)
-	}
-	if hello != "" {
-		err = c.Hello(hello)
-		if err != nil {
-			return e.New(err)
-		}
-	}
-	if a != nil {
-		if ok, _ := c.Extension("STARTTLS"); ok {
-			if err = c.StartTLS(&tls.Config{InsecureSkipVerify: true}); err != nil {
-				return e.New(err)
-			}
-		}
-		found, _ := c.Extension("AUTH")
-		if a != nil && found {
-			if err = c.Auth(a); err != nil {
-				return e.New(err)
-			}
-		}
-	}
-	if err = c.Mail(from); err != nil {
-		return e.New(err)
-	}
-	for _, addr := range to {
-		if err = c.Rcpt(addr); err != nil {
-			return e.New(err)
-		}
-	}
-	w, err := c.Data()
-	if err != nil {
-		return e.New(err)
-	}
-	_, err = w.Write(msg)
-	if err != nil {
-		return e.New(err)
-	}
-	err = w.Close()
-	if err != nil {
-		return e.New(err)
-	}
-	err = c.Quit()
-	if err != nil {
-		return e.New(err)
-	}
-	return nil
-}
-
+// Command object executes conn.SetDeadline before every function calls.
 type Command struct {
 	Timeout time.Duration
 	Conn    net.Conn
@@ -90,6 +28,7 @@ type Command struct {
 
 type Return func(args ...interface{})
 
+// SetError finds a type that implements a error interface and set it to err.
 func SetError(retvals []reflect.Value, err error) {
 	if err == nil {
 		return
@@ -108,7 +47,9 @@ func SetError(retvals []reflect.Value, err error) {
 	}
 }
 
-func (c *Command) Exec(f interface{}, args ...interface{}) Return {
+// ExecTimeout executes f with arguments args and return a function of type Return.
+// Return must be called with the pointer to the return values.
+func (c *Command) ExecTimeout(timeout time.Duration, f interface{}, args ...interface{}) Return {
 	c.retvals = make(chan []reflect.Value)
 	val := reflect.ValueOf(f)
 	if val.Kind() != reflect.Func {
@@ -126,9 +67,17 @@ func (c *Command) Exec(f interface{}, args ...interface{}) Return {
 		}
 	}
 	go func() {
-		err := c.Conn.SetDeadline(time.Now().Add(c.Timeout))
-		if err != nil {
-			log.Println("SetDeadline failed with error:", e.Trace(e.Forward(err)))
+		var err error
+		if timeout != 0 {
+			err = c.Conn.SetDeadline(time.Now().Add(timeout))
+			if err != nil {
+				log.Println("SetDeadline failed with error:", e.Trace(e.Forward(err)))
+			}
+		} else {
+			err = c.Conn.SetDeadline(time.Time{})
+			if err != nil {
+				log.Println("SetDeadline failed with error:", e.Trace(e.Forward(err)))
+			}
 		}
 		retvals := val.Call(a)
 		SetError(retvals, err)
@@ -152,6 +101,106 @@ func (c *Command) Exec(f interface{}, args ...interface{}) Return {
 			val.Elem().Set(retval)
 		}
 	}
+}
+
+// Exec executes f with arguments args and return a function of type Return.
+// Return must be called with the pointer to the return values.
+func (c *Command) Exec(f interface{}, args ...interface{}) Return {
+	return c.ExecTimeout(c.Timeout, f, args...)
+}
+
+// Generate a comma separated list of e-mails from a array of e-mails
+func EmailsToString(mails []string) (s string) {
+	for i, mail := range mails {
+		if i > 0 {
+			s += ", "
+		}
+		s += mail
+	}
+	return
+}
+
+// SendMail send a message to specific destination (to) using smtp server in addrs
+// and a auth.
+func SendMail(addr string, a smtp.Auth, from string, to []string, hello string, msg []byte, timeout time.Duration) error {
+	conn, err := net.DialTimeout("tcp", addr, timeout)
+	if err != nil {
+		return e.Forward(err)
+	}
+
+	command := &Command{
+		Timeout: timeout,
+		Conn:    conn,
+	}
+
+	var c *smtp.Client
+	r := command.Exec(smtp.NewClient, conn, addr)
+	r(&c, &err)
+	if err != nil {
+		return e.Forward(err)
+	}
+
+	if hello != "" {
+		r = command.Exec(c.Hello, hello)
+		r(&err)
+		if err != nil {
+			return e.Forward(err)
+		}
+	}
+
+	if a != nil {
+		if ok, _ := c.Extension("STARTTLS"); ok {
+			r = command.Exec(c.StartTLS, &tls.Config{InsecureSkipVerify: true})
+			r(&err)
+			if err != nil {
+				return e.Forward(err)
+			}
+		}
+		found, _ := c.Extension("AUTH")
+		if a != nil && found {
+			r = command.Exec(c.Auth, a)
+			r(&err)
+			if err != nil {
+				return e.Forward(err)
+			}
+		}
+	}
+
+	r = command.Exec(c.Mail, from)
+	r(&err)
+	if err != nil {
+		return e.Forward(err)
+	}
+
+	for _, addr := range to {
+		r = command.Exec(c.Rcpt, addr)
+		r(&err)
+		if err != nil {
+			return e.New(err)
+		}
+	}
+
+	var w io.WriteCloser
+	r = command.ExecTimeout(0, c.Data)
+	r(&w, &err)
+	if err != nil {
+		return e.New(err)
+	}
+	_, err = w.Write(msg)
+	if err != nil {
+		return e.New(err)
+	}
+	err = w.Close()
+	if err != nil {
+		return e.New(err)
+	}
+
+	r = command.Exec(c.Quit)
+	r(&err)
+	if err != nil {
+		return e.New(err)
+	}
+	return nil
 }
 
 // TestSMTP tests if can connect with the server and send some commands.
