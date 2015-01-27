aabf905d974337971c38a65935bf22b3b846fc17
Felipe Cavani <fcavani@gmail.com>
2015-01-27 11:14:28 -0200

    Changed the regexp for ipv6 and ipv4.

--- net/net.go
+++ net/net.go
@@ -9,7 +9,6 @@ import (
 	"github.com/fcavani/e"
 	"regexp"
 	"strings"
-	"net"
 )
 
 const ErrCantGetIp = "can't get remote ip"
@@ -17,48 +16,73 @@ const ErrCantSplitHostPort = "can't split host port"
 const ErrCantFindHost = "can't find the host"
 const ErrCantFindPort = "can't find the port number"
 
+type RegularExp string
+
+func (r RegularExp) Clean() string {
+	buf := make([]byte, len(Ipv6Regex))
+	j := 0
+	for _, re := range r {
+		if re != '\n' && re != '\t' && re != ' '{
+			if re > 255 {// rune is int32 
+				panic("no multibyte characters")
+			}
+			buf[j] = byte(re)
+			j++
+		}
+	}
+	buf = buf[:j]
+	return string(buf)
+}
+
+//Regex from https://gist.github.com/syzdek/6086792
+var Ipv6Regex RegularExp = `(
+	(([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))|
+	(::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))|
+	(fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,})|
+	(:((:[0-9a-fA-F]{1,4}){1,7}|:))|
+	(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}) |
+	([0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6}))|
+	([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|
+	(([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4})|
+	(([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3})|
+	(([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2})|
+	(([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4})|
+	(([0-9a-fA-F]{1,4}:){1,7}:)
+)`
+
+//Regex from https://gist.github.com/syzdek/6086792
+var Ipv4Regex RegularExp = `(
+	(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}
+	(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]
+)`
+
+var reCompIpv4 *regexp.Regexp
+var reCompIpv6 *regexp.Regexp
+var reCompIpv6Port *regexp.Regexp
+
+func init() {
+ 	ipv6re := Ipv6Regex.Clean()
+	ipv6portre := `\[`+ipv6re+`\]\:([0-9]*)`
+	
+	reCompIpv4 = regexp.MustCompile(Ipv4Regex.Clean())
+	reCompIpv6 = regexp.MustCompile(ipv6re)
+	reCompIpv6Port = regexp.MustCompile(ipv6portre)
+}
+
 func IsValidIpv4(ip string) bool {
-	r, err := regexp.Compile(`^(([0-9]{1,3}\.){3}[0-9]{1,3})$`)
-	if err != nil {
-		panic("can't compile ipv6 regexp")
-	}
-	x := r.FindAllStringSubmatch(ip, -1)
-	if len(x) == 0 {
-		return false
-	}
-	if len(x[0]) != 3 {
-		return false
-	}
-	if x[0][1] != ip {
-		return false
-	}
-	ipParsed := net.ParseIP(ip)
-	if ipParsed == nil {
+	if ip != reCompIpv4.FindString(ip) {
 		return false
 	}
 	return true
 }
-	
+
 func IsValidIpv6(ip string) bool {
-	r, err := regexp.Compile(`\[([a-z0-9:]*)\]`)
-	if err != nil {
-		panic("can't compile ipv6 regexp")
-	}
-	x := r.FindAllStringSubmatch(ip, -1)
-	if len(x) == 0 {
-		return false
-	}
-	if len(x[0]) != 2 {
-		return false
-	}
 	ip = strings.TrimSuffix(strings.TrimPrefix(ip, "["), "]")
-	if x[0][1] !=  ip {
+
+	if ip != reCompIpv6.FindString(ip) {
 		return false
 	}
-	ipParsed := net.ParseIP(ip)
-	if ipParsed == nil {
-		return false
-	}
+	
 	return true
 }
 
@@ -69,11 +93,7 @@ func SplitHostPort(hp string) (host, port string, err error) {
 	}
 	if hp[0] == '[' {
 		// ipv6 - [2001:db8:1f70::999:de8:7648:6e8]:100
-		r, err := regexp.Compile(`\[([a-z0-9:]*)\]\:([0-9]*)`)
-		if err != nil {
-			return "", "", e.Push(err, "can't compile ipv6 regexp")
-		}
-		x := r.FindAllStringSubmatch(hp, -1)
+		x := reCompIpv6Port.FindAllStringSubmatch(hp, -1)
 		if len(x) == 0 {
 			if IsValidIpv6(hp) {
 				host = strings.TrimSuffix(strings.TrimPrefix(hp, "["), "]")
@@ -82,12 +102,12 @@ func SplitHostPort(hp string) (host, port string, err error) {
 				return "", "", e.New(ErrCantGetIp)
 			}
 		} else {
-			if len(x[0]) == 2 {
+			if len(x[0]) >= 3 {
+				host = x[0][1]
+				port = x[0][len(x[0])-1] //Last is the port
+			} else if len(x[0]) >= 2 {
 				host = x[0][1]
 				port = ""
-			} else if len(x[0]) == 3 {
-				host = x[0][1]
-				port = x[0][2]
 			} else {
 				return "", "", e.New(ErrCantGetIp)
 			}
--- net/net_test.go
+++ net/net_test.go
@@ -9,12 +9,12 @@ import (
 	"testing"
 )
 
-type testipv4struct struct {
-	ip string
+type testipstruct struct {
+	ip    string
 	valid bool
 }
 
-var testipv4 []testipv4struct = []testipv4struct{
+var testipv4 []testipstruct = []testipstruct{
 	{"127.0.0.1", true},
 	{"10.0.0.1", true},
 	{"192.168.1.1", true},
@@ -22,6 +22,8 @@ var testipv4 []testipv4struct = []testipv4struct{
 	{"255.255.255.255", true},
 	{"300.2.2.2", false},
 	{"ab.cb.3.123", false},
+	{"catoto", false},
+	{"192.168.10.1a", false},
 }
 
 func TestIsValidIpv4(t *testing.T) {
@@ -32,6 +34,57 @@ func TestIsValidIpv4(t *testing.T) {
 		}
 	}
 }
+
+var testipv6 []testipstruct = []testipstruct{
+	{"1:2:3:4:5:6:7:8", true},
+	{"1::", true},
+	{"1:2:3:4:5:6:7::", true},
+	{"1::8", true},
+	{"1:2:3:4:5:6::8", true},
+	{"1::7:8", true},
+	{"1:2:3:4:5::7:8", true},
+	{"1:2:3:4:5::8", true},
+	{"1::6:7:8", true},
+	{"1:2:3:4::6:7:8", true},
+	{"1:2:3:4::8", true},
+	{"1::5:6:7:8", true},
+	{"1:2:3::5:6:7:8", true},
+	{"1:2:3::8", true},
+	{"1::4:5:6:7:8", true},
+	{"1:2::4:5:6:7:8", true},
+	{"1:2::8", true},
+	{"1::3:4:5:6:7:8", true},
+	{"1::3:4:5:6:7:8", true},
+	{"1::8", true},
+	{"::2:3:4:5:6:7:8", true},
+	{"::2:3:4:5:6:7:8", true},
+	{"::8", true},
+	{"::", true},
+	{"fe80::7:8%eth0", true},
+	{"fe80::7:8%1", true},
+	{"::255.255.255.255", true},
+	{"::ffff:255.255.255.255", true},
+	{"::ffff:0:255.255.255.255", true},
+	{"2001:db8:3:4::192.0.2.33", true},
+	{"64:ff9b::192.0.2.33", true},
+	{"2001:db8:1f70::999:de8:7648:6e8", true},
+	{"catodos", false},
+	{"192.168.1.1", false},
+	{"2001:db8:1f70::999:de8:7648:6e8z", false},
+	{"2001:db8:1f70:0:999:de8:7648:6e8", true},
+	{"2001:db8:1f70:x:999:de8:7648:6e8", false},
+}
+
+func TestIsValidIpv6(t *testing.T) {
+	for i, ip := range testipv6 {
+		t.Log(i, ip.ip)
+		ok := IsValidIpv6(ip.ip)
+		if ok != ip.valid {
+			t.Fatal("failed for", i, ip.ip)
+		}
+	}
+}
+
 
 type testHostPortStruct struct {
 	hostport string
